29f496c9abb1cfaf712385668c735eea01e2b6e7
David John Hulatt <74449038+davidjohn1960@users.noreply.github.com>
2024-06-25 15:11:59 +0100

    Quartz sync: Jun 25, 2024, 3:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "David John Hulatt",
+    pageTitle: "David Hulatt",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -59,7 +59,7 @@ const config: QuartzConfig = {
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
-          dark: "github-dark",
+          dark: "material-theme-ocean",
         },
         keepBackground: false,
       }),
