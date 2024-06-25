70a60a113d4ccec585ed5d1b50bfae3c0aef4db1
David John Hulatt <74449038+davidjohn1960@users.noreply.github.com>
2024-06-25 14:38:57 +0100

    Quartz sync: Jun 25, 2024, 2:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -61,7 +61,7 @@ const config: QuartzConfig = {
           light: "github-light",
           dark: "github-dark",
         },
-        keepBackground: true,
+        keepBackground: false,
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
