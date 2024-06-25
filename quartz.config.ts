a05d2a1867e62d55b08fdad1b9ec4f6dd4c57da0
David John Hulatt <74449038+davidjohn1960@users.noreply.github.com>
2024-06-25 14:41:34 +0100

    Quartz sync: Jun 25, 2024, 2:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-GB",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
